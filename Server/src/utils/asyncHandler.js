40825be61332f9d44475447de9dfa91404a1ab45
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-02-16 11:10:27 +0530

    add custom API [Response & Error] Handler

--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,25 +1,9 @@
 const asyncHandler = (requestHandler) => async (req, res, next) => {
 
-    // Use Promise 
     (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
             .catch((error) => next(error))
     }
-
-
-
-
-    // try {
-    //     await fn(req, res, next)
-    // } catch (error) {
-    //     res.status(error.code || 500).json(
-    //         {
-    //             success: false,
-    //             message: error.message
-    //         }
-    //     )
-    // }
-
 
 }
 
