79fa15d950b2176c871381612fc1b4f84fa57d21
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-02-16 10:48:56 +0530

    add async request handler function another approach [use promise]

--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,12 +1,26 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json(
-            {
-                success: false,
-                message: error.message
-            }
-        )
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+
+    // Use Promise 
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
     }
+
+
+
+
+    // try {
+    //     await fn(req, res, next)
+    // } catch (error) {
+    //     res.status(error.code || 500).json(
+    //         {
+    //             success: false,
+    //             message: error.message
+    //         }
+    //     )
+    // }
+
+
 }
+
+export { asyncHandler };
